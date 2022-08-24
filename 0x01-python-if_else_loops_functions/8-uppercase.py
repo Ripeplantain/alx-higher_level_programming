5cfdbce8560e22918bf0da373793dbc058efcc3c
Emmanuel Gyang <irvingmanny@gmail.com>
2022-08-24 11:27:02 +0000

    stuff to do

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        uni_code = ord(str[i])
-        if uni_code >= 97 and uni_code <= 122:
-            uni_code = uni_code - 32
-        print(f"{chr(unicode)}")
-    print()
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
+
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
