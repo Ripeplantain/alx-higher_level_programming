77d9e1025c764cd93da5438738140e5371d21dd9
Emmanuel Gyang <irvingmanny@gmail.com>
2022-08-24 11:19:07 +0000

    stuff to do

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(0,10):
-        print(f"{i}{j}")
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
