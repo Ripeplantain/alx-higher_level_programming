219af72c8de5dd2e591ca9613a3dee92227180d8
Emmanuel Gyang <irvingmanny@gmail.com>
2022-08-25 16:45:51 +0000

    things and things

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""Print sum of 1 and 2_"""
-	from add_0 import add
+    """Print some of 1 and 2_"""
+    from add_0 import add
 
-	a=1
-	b=2
-	print("{} + {} = {}".format(a,b,add(a,b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
