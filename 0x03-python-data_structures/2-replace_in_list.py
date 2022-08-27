25f967a96ae567d80284a21e5bbcf09370ad5ecc
Ripeplantain <irvingmanny@gmail.com>
2022-08-27 12:52:40 +0000

    stuff to do

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list,idx,element):
-        """Replaces element at an index in a list"""
-	if 0 <= idx < len(my_list):
-		my_list[idx] = element
-	return(my_list)
+    """replaces element at an index in a list"""
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+	    return(my_list)
