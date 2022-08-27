e35ae044704ad3c605a942132136e4a656ca181e
Ripeplantain <irvingmanny@gmail.com>
2022-08-27 12:56:44 +0000

    student loans

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list,idx,element):
-    """replaces element at an index in a list"""
     if 0 <= idx < len(my_list):
         my_list[idx] = element
     return(my_list)
