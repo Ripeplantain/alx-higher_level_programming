c3740544a6c9fce6e522d7d2c6416a92eb7a926e
Ripeplantain <irvingmanny@gmail.com>
2022-08-28 00:03:06 +0000

    first and last

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """Returns length of a string and the first character"""
     length = len(sentence)
     first_char = sentence[0] if length > 0 else "None"
     tup = length, first_char
