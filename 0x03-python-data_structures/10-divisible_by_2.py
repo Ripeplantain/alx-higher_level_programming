b7ed8f5592e2b88eed92f44987bff3aa98ffe1cd
Ripeplantain <irvingmanny@gmail.com>
2022-08-29 01:35:01 +0000

    stuff

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for list in my_list:
+    if i % 2 == 0:
+	return "{} is divisible by 2".format(i)
+     return "{} is not divisible by 2".format(i)
