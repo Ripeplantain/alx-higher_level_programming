f8e9995dab5b8f86883e2d431a96b23febf8d37c
Ripeplantain <irvingmanny@gmail.com>
2022-09-01 10:25:27 +0000

    square a matrix with lambda

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    temporary = []
+    for x in matrix:
+        temporary.append(list(map(lambda x: x*x,matrix)))
+        return temporary
