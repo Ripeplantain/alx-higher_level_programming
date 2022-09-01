e13c3b5e3538fe3c3dd1fab53619e82887dfafd2
Ripeplantain <irvingmanny@gmail.com>
2022-09-01 10:26:52 +0000

    again

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     temporary = []
     for x in matrix:
-        temporary.append(list(map(lambda x: x*x,matrix)))
+        temporary.append(list(map(lambda x: x**2,x)))
         return temporary
