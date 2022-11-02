ffa4de92a9909538b52e6d4653aba4fcad7c5ae0
Ripeplantain <irvingmanny@gmail.com>
2022-11-02 19:04:12 +0000

    made some changes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,11 @@
---full table creation with other amenities
-CREATE TABLE IF NOT EXIST second_table (id int,name varchar(256),score int);
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+-- second_table description:
+--              id INT
+--              name VARCHAR(256)
+--              score INT
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
 (2, "Alex", 3),
