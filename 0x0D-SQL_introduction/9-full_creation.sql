7419c4ce1e76be6b9013a40802591eca255cfbba
Ripeplantain <irvingmanny@gmail.com>
2022-11-02 18:49:38 +0000

    insertion

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 --full table creation with other amenities
 CREATE TABLE IF NOT EXIST second_table (id int,name varchar(256),score int);
-INSERT INTO second_table (id,name,score) VALUES (1,"John",10)
-INSERT INTO second_table (id,name,score) VALUES (2,"John",3)
-INSERT INTO second_table (id,name,score) VALUES (3,"John",14)
-INSERT INTO second_table (id,name,score) VALUES (4,"John",8)
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
