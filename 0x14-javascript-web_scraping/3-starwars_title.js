b5e1033538055914a5a50fe274ffcc6173122276
Ripeplantain <irvingmanny@gmail.com>
2023-01-17 18:00:08 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Get star wars title
+
+const request = require('request');
+const url = ' https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error', error);
+  }
+  console.log(JSON.parse(body).title);
+});
