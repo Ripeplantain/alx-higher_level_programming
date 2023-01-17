955f07c57d9cf456a54b607bb1f3592c7cfa0c37
Ripeplantain <irvingmanny@gmail.com>
2023-01-17 17:23:14 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// Print the status code of a get request
+// A script that display the status code of a GET request
 
-const request = require('request');
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error){
-    console.error('Error: ', error);
-  }
-  console.log('code: ', response && response.statusCode);
+const args = process.argv;
+let request = require('request');
+request(args[2], function (error, response, body) {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  } else console.log('code:', response && response.statusCode);
 });
